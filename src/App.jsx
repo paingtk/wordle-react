1b64c81f0f92bf6ab13dc94a874278a084db3eeb
Paing Thu Kyaw <paingthukyaw534@gmail.com>
2023-02-07 13:55:02 +0630

    add solution words and generate random solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+  const [solution, setSolution] = useState(null)
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then((res) => res.json())
+      .then((data) => {
+        const randomSolution = data[Math.floor(Math.random() * data.length)]
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution])
   return (
     <div>
       <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   )
 }
