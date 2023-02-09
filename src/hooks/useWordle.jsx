3596edbeca70b7afbce8417fab6f45a5197bc312
Paing Thu Kyaw <paingthukyaw534@gmail.com>
2023-02-09 18:25:04 +0630

    check conditions to format current guess word

--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -4,11 +4,13 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0) // track what player turn is on.
   const [currentGuess, setCurrentGuess] = useState('')
   const [guesses, setGuesses] = useState([]) // each guess is an array
-  const [history, setHistory] = useState([]) // each guess is a string
+  const [history, setHistory] = useState(['apple', 'spare']) // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false)
 
   // format player guess word into an array of objects. e.g. [{letter: 'a', color: 'yellow'}]
-  const formatGuess = () => {}
+  const formatGuess = () => {
+    console.log('formatted guess is -', currentGuess)
+  }
 
   // add player new guess word to the guesses state.
   // update the isCorrect state if the guess is right.
@@ -18,6 +20,26 @@ const useWordle = (solution) => {
   // handle keyup and track the current guess word.
   // if player presses enter, add the new guess word.
   const handleKeyup = ({ key }) => {
+    if (key === 'Enter') {
+      // check if the turn is over 5
+      if (turn > 5) {
+        console.log('used all the turn')
+        return
+      }
+      // check if the guess word is already submitted
+      if (history.includes(currentGuess)) {
+        console.log('This word is already guessed!')
+        return
+      }
+      // check if word is 5 char long
+      if (currentGuess.length !== 5) {
+        console.log('word must be 5 char long')
+        return
+      }
+      // call formatGuess function if above condition are passed
+      formatGuess()
+    }
+
     if (key === 'Backspace') {
       setCurrentGuess((prev) => {
         return prev.slice(0, -1) // remove the last letter
